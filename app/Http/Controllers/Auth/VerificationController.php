ba365ef9bf97cdcac3b19020cfff4af74befa616
may <bingqing0822@163.com>
2023-09-11 13:55:16 +0800

    强制用户认证-邮箱

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -35,8 +35,12 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+        // 所有的控制器动作都需要登录后才能访问
         $this->middleware('auth');
+        // 只有 verify 动作使用 signed 中间件进行认证
         $this->middleware('signed')->only('verify');
+        // throttle 中间件是框架提供的访问频率限制功能，throttle 中间件会接收两个参数，这两个参数决定了在给定的分钟数内可以进行的最大请求数。
+        // 对 verify 和 resend 动作做了频率限制,限定了这两个动作访问频率是 1 分钟内不能超过 6 次
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
